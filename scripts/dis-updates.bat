37afe93e8c1fa7535704211e24f7e2df5de4684e
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-22 20:52:53 +0200

    reg delete key /f to force without prompt

--- scripts/dis-updates.bat
+++ scripts/dis-updates.bat
@@ -2,7 +2,7 @@ rem http://www.windows-commandline.com/disable-automatic-updates-command-line/
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
 
 rem remove optional WSUS server settings
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
 
 rem even harder, disable windows update service
 rem sc config wuauserv start= disabled
