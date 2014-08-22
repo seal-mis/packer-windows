8f84dfc34e98fb36e6107eb4ced7277dd9387537
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-22 17:33:41 +0200

    added optional WSUS server, remove settings at the end again

--- scripts/dis-updates.bat
+++ scripts/dis-updates.bat
@@ -1,6 +1,9 @@
 rem http://www.windows-commandline.com/disable-automatic-updates-command-line/
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
 
+rem remove optional WSUS server settings
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+
 rem even harder, disable windows update service
 rem sc config wuauserv start= disabled
 rem net stop wuauserv
